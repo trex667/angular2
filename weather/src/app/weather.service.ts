644eba7116400538e7ef85c86dc87ae7768ecce3
trex667 <froehlich.ch@gmail.com>
2016-11-24 21:06:17 +0100

    feat: add weather service

---
+++ weather/src/app/weather.service.ts
@@ -0,0 +1,32 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import {Forecast} from './entities';
+import { Observable }     from 'rxjs/Observable';
+
+@Injectable()
+export class HeroService {
+  let url = "http://api.openweathermap.org/data/2.5/forecast?q=Trier,de&mode=json&units=metric&lang=de&APPID=a59c9a186c9eddca27a2f6f157d45275";
+  constructor (private http: Http) {}
+  getForecast (): Observable<Forecast[]> {
+    return this.http.get(this.url)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+  private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
